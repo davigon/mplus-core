8cd81cbcfd946aeb5e5b3a74bd5d378f4b36afcb
davigon <dvgnlz@outlook.es>
2022-06-11 17:37:15 +0200

    Add Programme domain class

---
+++ src/main/java/com/github/davigon/domain/Programme.java
@@ -0,0 +1,29 @@
+package com.github.davigon.domain;
+
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+public class Programme {
+    private final OffsetDateTime start;
+
+    private final OffsetDateTime stop;
+
+    private final String channel;
+
+    private final String title;
+
+    private final String description;
+
+    private final String category;
+
+    public Programme(OffsetDateTime start, OffsetDateTime stop, String channel, String title, String description, String category) {
+        this.start = start;
+        this.stop = stop;
+        this.channel = channel;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+    }
+}
