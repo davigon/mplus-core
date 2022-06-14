5929224e4eee1981f44e7a0e2987d05937ba7d94
davigon <dvgnlz@outlook.es>
2022-06-14 20:01:44 +0200

    Fix root xmltv element

--- src/main/java/com/github/davigon/services/data/Xmltv.java
+++ src/main/java/com/github/davigon/services/data/Xmltv.java
@@ -2,10 +2,12 @@ package com.github.davigon.services.data;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@JacksonXmlRootElement(localName = "tv")
 public record Xmltv(
         @JacksonXmlProperty(isAttribute = true, localName = "date")
         LocalDate date,
